564d88f3055e4d41ffa880fcedca519d1ac2440e
Abhijeet Lodhiyal <abhijeetlodhiyal@gmail.com>
2023-02-06 15:46:38 +0530

    Deploy MIllion

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './OpeningPage/App'
 import './index.css'
@@ -9,49 +9,34 @@ import SearchPage from './SearchedPage/SearchPage';
 import VideoWatchPage from './WatchPage/VideoWatchPage';
 import Search from './Search';
 import ShowShort from './Shorts/ShowShort';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/search/:searchValue",
-    element: <>
-      <Header />
-      <div className='flex'>
-        <Sidebar />
-        <SearchPage /> 
-      </div>
-    </>
-  },
-  {
-    path: "/search/",
-    element: <>
-      <Search />
-    </>
-  },
-  {
-    path:"/watch",
-    element:<>
-      <Header />
-      <VideoWatchPage />
-    </>
-  },
-  {
-    path:"/shorts",
-    element:<>
-      <Header />
-      <div className='flex relative'>
-        <Sidebar />
-        <ShowShort />
-      </div>
-     
-    </>
-  }
-])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <HashRouter>
+    <Routes >
+      <Route path='/' element={<App />} />
+      <Route path='/search/:searchValue' element={<>
+        <Header />
+        <div className='flex'>
+          <Sidebar />
+          <SearchPage />
+        </div>
+      </>} />
+      <Route path='/search/' element={<Search />} />
+      <Route path='/watch' element={<>
+        <Header />
+        <VideoWatchPage />
+      </>} />
+      <Route path='/shorts' element={<>
+        <Header />
+        <div className='flex relative'>
+          <Sidebar />
+          <ShowShort />
+        </div>
+      </>} />
+      </Routes>
+    </HashRouter>
+
   </React.StrictMode>,
 )
